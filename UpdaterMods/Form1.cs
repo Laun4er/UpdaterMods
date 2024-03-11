33110d3dd64e585dd9d897a72f2d92a36a53c8d6
Laun4er <Laun4er@DESKTOP-O9MD4KJ>
2024-03-11 07:25:05 +0500

    Добавьте файлы проекта.

---
+++ UpdaterMods/Form1.cs
@@ -0,0 +1,12 @@
+namespace UpdaterMods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+        }
+    }
+}
